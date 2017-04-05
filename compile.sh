99415af1d69da71876d877265d54888136f905da
Stu Levine <stuart.levine@me.com>
2017-04-05 12:15:40 +0000

    - Added pi calculation example with display - Using BigInt library and SipHash to allow pi calculation with big ints - moved all external libraries to the lib directory

--- compile.sh
+++
@@ -1,6 +0,0 @@
-#!env /bin/sh
-#
-# Since the Raspberry Pi does not currently have a working Package Manager, we need
-# to pull it all together into a compile script
-#
-swiftc -v -o ledfun -I SMBus-swift/Packages/Ci2c -I SMBus-swift/Packages/CioctlHelper SMBus-swift/Sources/SMBus.swift LEDBackpack.swift main.swift
